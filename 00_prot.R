bac858dc967ce4e3af3d4645f03372f8c1241c66
Nicolas Iderhoff <nicolasiderhoff@googlemail.com>
2016-07-10 14:37:29 +0200

    added code for colourful dendrogram

--- 00_prot.R
+++ 00_prot.R
@@ -12,6 +12,7 @@ library(vegan)
 
 ## Number of clusters
 n_cluster = 10 #TODO: why 10?
+n_countries = 194 #TODO: dynamic
 
 # Run different measures
 # * Gower, Jaccard, Kulczynski, Mountford, Euclidean, Canberra, Manhatten
@@ -24,9 +25,32 @@ cluster_table = jaccard_clust # full table incl. cluster nos
 # As well as
 # * Divisive Clustering
 # * PAM
-
 # For each generate:
 # * List of cluster names (get_names())
 # * Means for all clusters (get_means())
-# * plt: Worldmap ("final_map.R")
-# * plt: Heatmap ("heatmap.R")
+
+# Plot Worldmap
+
+# Plot Heatmap
+
+# Plot Dendrogram
+library(dendextend)
+library(RColorBrewer)
+dendrogram = color_branches(
+    dend = as.dendrogram(jaccard_hclust),
+    k = n_cluster,
+#    col = brewer.pal(n_cluster, "Paired")
+    col = c("tomato","slateblue")
+)
+labels(dendrogram) = rep(" ", n_countries)
+dendrogram = assign_values_to_branches_edgePar(
+    dend = dendrogram, value = 2, edgePar = "lwd"
+)
+
+par(mar=c(4.5, 5.4, 2.3, 1.0))
+plot(dendrogram, ylab = "Jaccard Similarty Coefficient",
+     cex.lab = 1.5,
+     cex.axis = 1.5)
+
+cutoff = get_branches_heights(dendrogram, sort = T, decreasing = T)[n_cluster-1]
+abline(h=cutoff, lwd = 2, lty = "dashed", col = "gray")
--- 05_jaccard.R
+++ 05_jaccard.R
@@ -5,9 +5,9 @@
 jaccard_measure = vegdist(bin_dat, method="jaccard")
 jaccard_hclust = hclust(jaccard_measure)
 
-#plot(jaccard_hclust,
-#     main = c("Jaccard proximity measure & hierarchical algorithm"),
-#     hang = -1)
+# plot(jaccard_hclust,
+#      main = c("Jaccard proximity measure & hierarchical algorithm"),
+#      hang = -1)
 
 jaccard_cut = cutree(jaccard_hclust, k = n_cluster)
 jaccard_clust = cbind(dat_cluster, "cluster" = jaccard_cut)
